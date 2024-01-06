57113a3d9fe7197b3afe38221454fd1aee44fd38
MrManavRamani-Flutter <manavramani772@gmail.com>
2024-01-06 13:26:26 +0530

    create appbar and bottom bar with simple design

--- lib/modules/views/screens/home_page.dart
+++ lib/modules/views/screens/home_page.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:media_booster/modules/components/app_bar/custom_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('data');
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.deepPurple.shade900.withOpacity(0.8),
+            Colors.deepPurple.shade300.withOpacity(0.8),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: const CustomAppBar(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.deepPurple.shade800,
+          unselectedItemColor: Colors.white,
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+          selectedItemColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_fill_outlined),
+              label: 'Play',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_outline),
+              label: 'Profile',
+            ),
+          ],
+        ),
+        body: Container(),
+      ),
+    );
   }
 }
